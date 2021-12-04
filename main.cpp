8b4ad8a792f7ea32b2a7469b8f0b53fb1634a10b
veringsek <veringsek@gmail.com>
2021-12-04 13:25:03 +0800

    kill old indicator window

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 // Globals
 HWINEVENTHOOK hook;
-HWND hwndWindow;
 
 // Header
 void terminates();
@@ -21,29 +20,7 @@ void CALLBACK WinEventProc(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idOb
 {
     RECT windowTarget;
     GetWindowRect(hwnd, &windowTarget);
-    if (event == EVENT_SYSTEM_MOVESIZESTART)
-    {
-        RECT workarea;
-        SystemParametersInfoA(SPI_GETWORKAREA, 0, &workarea, 0);
-        POINT center;
-        center.x = (workarea.left + workarea.right) / 2;
-        center.y = (workarea.top + workarea.bottom) / 2;
-        RECT windowIndicator;
-        GetWindowRect(hwndWindow, &windowIndicator);
-        // int width = windowTarget.right - windowTarget.left;
-        // int height = windowTarget.bottom - windowTarget.top;
-        int width = 100 * 2; 
-        int height = 100 * 2;
-        SetWindowPos(hwndWindow, HWND_TOP, center.x - width / 2, center.y - height / 2, width, height, SWP_SHOWWINDOW);
-        // ShowWindow(hwndWindow, SW_RESTORE);
-        SetForegroundWindow(hwndWindow);
-        SetFocus(hwndWindow);
-        SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
-        SetFocus(hwnd);
-    }
-    else
-    {
-        ShowWindow(hwndWindow, SW_HIDE);
+    if (event == EVENT_SYSTEM_MOVESIZEEND) {
         POINT cursor;
         GetCursorPos(&cursor);
         RECT workarea;
@@ -74,34 +51,9 @@ BOOL WINAPI consoleControlHandler(DWORD sig)
     return true;
 }
 
-const TCHAR CLASS_NAME[] = TEXT("WindowsSnapCenterClass");
-LRESULT CALLBACK WinProc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp);
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
-
 int main()
 {
-    // FreeConsole();
-    WNDCLASS windowClass = {0};
-    windowClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
-    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    windowClass.hInstance = NULL;
-    windowClass.lpfnWndProc = WndProc;
-    windowClass.lpszClassName = TEXT("Window in Console");
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
-    if (!RegisterClass(&windowClass))
-        MessageBox(NULL, TEXT("Could not register class"), TEXT("Error"), MB_OK);
-    hwndWindow = CreateWindow(TEXT("Window in Console"),
-                              NULL,
-                              WS_POPUP,
-                              0,
-                              0,
-                              100,
-                              100,
-                              NULL,
-                              NULL,
-                              NULL,
-                              NULL);
+    FreeConsole();
     SetConsoleCtrlHandler(consoleControlHandler, TRUE);
     Hook();
 
@@ -111,32 +63,7 @@ int main()
         TranslateMessage(&messages);
         DispatchMessage(&messages);
     }
-    DeleteObject(hwndWindow); //doing it just in case
     return messages.wParam;
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
-{
-    switch (message)
-    {
-    case WM_CHAR: //this is just for a program exit besides window's borders/task bar
-        if (wparam == VK_ESCAPE)
-        {
-            DestroyWindow(hwnd);
-        }
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    case WM_MOUSEMOVE:
-        POINT cursor;
-        GetCursorPos(&cursor);
-        // cout << cursor.x << ", " << cursor.y << endl;
-        MessageBox(NULL, TEXT("CCC"), TEXT("Error"), MB_OK);
-        break;
-    default:
-        return DefWindowProc(hwnd, message, wparam, lparam);
-    }
-    return 0;
 }
 
 void terminates()
