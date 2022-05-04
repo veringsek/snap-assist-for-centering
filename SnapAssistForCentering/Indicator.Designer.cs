543ee532387aa9eacc10052572006d16dfd7dc60
veringsek <veringsek@gmail.com>
2022-05-04 22:23:19 +0800

    support multi screens

--- SnapAssistForCentering/Indicator.Designer.cs
+++ SnapAssistForCentering/Indicator.Designer.cs
@@ -40,7 +40,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.IndianRed;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(96, 96);
             this.ControlBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
--- SnapAssistForCentering/Indicator.cs
+++ SnapAssistForCentering/Indicator.cs
@@ -9,9 +9,6 @@ namespace SnapAssistForCentering
         {
             InitializeComponent();
 
-            // Initialize for Acrylic
-            //SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-
             hwndDragging = IntPtr.Zero;
             procDelegate = new WinEventDelegate(WinEventProc);
             IntPtr hook = SetWinEventHook(EVENT_SYSTEM_MOVESIZESTART, EVENT_SYSTEM_MOVESIZEEND, IntPtr.Zero, procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
@@ -19,7 +16,7 @@ namespace SnapAssistForCentering
 
         private void Indicator_Load(object sender, EventArgs e)
         {
-            Acrylic.Enable(this.Handle);
+            Acrylic.Enable(Handle);
         }
 
         private void Indicator_Shown(object sender, EventArgs e)
@@ -37,10 +34,17 @@ namespace SnapAssistForCentering
         {
             if (hwndDragging == IntPtr.Zero) return;
 
-            Rectangle rect = new Rectangle();
+            Screen? screen = CursorScreen();
+            if (screen == null) return;
+
+            Rectangle rect = new();
             GetWindowRectangle(hwndDragging, out rect);
-            Rectangle sensor = new Rectangle(-SENSOR_WIDTH / 2, -SENSOR_HEIGHT / 2, SENSOR_WIDTH, SENSOR_HEIGHT);
-            sensor.Offset(new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2));
+
+            Rectangle sensor = Centering(screen.Bounds, new Size(SENSOR_WIDTH, SENSOR_HEIGHT));
+
+            //Rectangle sensor = new Rectangle(-SENSOR_WIDTH / 2, -SENSOR_HEIGHT / 2, SENSOR_WIDTH, SENSOR_HEIGHT);
+            //sensor.Offset(new Point(screen.Bounds.Width / 2, screen.Bounds.Height / 2));
+            //sensor.Offset(new Point(screen.Bounds.X, screen.Bounds.Y));
 
             if (sensor.Contains(Cursor.Position))
             {
@@ -50,7 +54,38 @@ namespace SnapAssistForCentering
             {
                 Size = new Size(100, 100);
             }
-            Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2) - Size / 2;
+            Location = LeftTop(Centering(screen.Bounds, Size));
+            //Location = new Point(screen.Bounds.Width / 2, screen.Bounds.Height / 2) - Size / 2;
+        }
+
+        private static Screen? CursorScreen()
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.Bounds.Contains(Cursor.Position))
+                {
+                    return screen;
+                }
+            }
+            return null;
+        }
+
+        private static Rectangle Centering(Rectangle bounds, Size size)
+        {
+            return new Rectangle(
+                bounds.X + bounds.Width / 2 - size.Width / 2,
+                bounds.Y + bounds.Height / 2 - size.Height / 2,
+                size.Width, size.Height);
+        }
+
+        private static Point LeftTop(Rectangle rectangle)
+        {
+            return new Point(rectangle.Left, rectangle.Top);
+        }
+
+        private static Size RectangleSize(Rectangle rectangle)
+        {
+            return new Size(rectangle.Width, rectangle.Height);
         }
 
         bool startup = true;
@@ -70,9 +105,13 @@ namespace SnapAssistForCentering
                 hwndDragging = hwnd;
                 tmrCursor.Start();
 
+                Screen? screen = CursorScreen();
+                if (screen == null) return;
+
                 IntPtr hwndIndicator = Process.GetCurrentProcess().Handle;
-                Size = new Size(100, 100);
-                Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2) - Size / 2;
+                Location = LeftTop(Centering(screen.Bounds, new Size(SENSOR_WIDTH, SENSOR_HEIGHT)));
+                //Size = new Size(100, 100);
+                //Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2) - Size / 2;
                 Show();
                 SetWindowPos(hwndIndicator, hwndDragging, Location.X, Location.Y, Size.Width, Size.Height, SW_SHOW);
             }
@@ -81,15 +120,21 @@ namespace SnapAssistForCentering
                 tmrCursor.Stop();
                 Hide();
 
-                Rectangle sensor = new Rectangle(-SENSOR_WIDTH / 2, -SENSOR_HEIGHT / 2, SENSOR_WIDTH, SENSOR_HEIGHT);
-                sensor.Offset(new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2));
+                Screen? screen = CursorScreen();
+                if (screen == null) return;
+
+                Rectangle sensor = Centering(screen.Bounds, new Size(SENSOR_WIDTH, SENSOR_HEIGHT));
+                //Rectangle sensor = new Rectangle(-SENSOR_WIDTH / 2, -SENSOR_HEIGHT / 2, SENSOR_WIDTH, SENSOR_HEIGHT);
+                //sensor.Offset(new Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2));
 
                 if (sensor.Contains(Cursor.Position))
                 {
-                    Rectangle rect = new Rectangle();
+                    Rectangle rect = new();
                     GetWindowRectangle(hwndDragging, out rect);
-                    Point destination = new Point((Screen.PrimaryScreen.Bounds.Width - rect.Width) / 2, (Screen.PrimaryScreen.Bounds.Height - rect.Height) / 2);
-                    SetWindowPos(hwndDragging, IntPtr.Zero, destination.X, destination.Y, rect.Width, rect.Height, SW_SHOW);
+                    Rectangle destination = Centering(screen.Bounds, RectangleSize(rect));
+                    SetWindowPos(hwndDragging, IntPtr.Zero, destination.X, destination.Y, destination.Width, destination.Height, SW_SHOW);
+                    //Point destination = new Point((Screen.PrimaryScreen.Bounds.Width - rect.Width) / 2, (Screen.PrimaryScreen.Bounds.Height - rect.Height) / 2);
+                    //SetWindowPos(hwndDragging, IntPtr.Zero, destination.X, destination.Y, rect.Width, rect.Height, SW_SHOW);
                 }
 
                 hwndDragging = IntPtr.Zero;
@@ -119,27 +164,12 @@ namespace SnapAssistForCentering
         [DllImport("user32.dll")]
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hwndInsertAfter, int x, int y, int cx, int cy, uint flags);
 
-
-
-        //internal void EnableBlur()
-        //{
-        //    AccentPolicy accent = new AccentPolicy();
-        //    accent.AccentState = AccentState.ACCENT_ACRYLIC_BLUR_BEHIND;
-        //    accent.AccentFlags = 0x20 | 0x40 | 0x80 | 0x100;
-        //    //accent.GradientColor = (intBlurOpacity << 24) | (intBlurOpacity & 0xFFFFFF);
-        //    var accentStructSize = Marshal.SizeOf(accent);
-        //    var accentPtr = Marshal.AllocHGlobal(accentStructSize);
-        //    Marshal.StructureToPtr(accent, accentPtr, false);
-        //    WindowCompositionAttributeData data = new WindowCompositionAttributeData();
-        //    data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
-        //    data.SizeOfData = accentStructSize;
-        //    data.Data = accentPtr;
-        //    SetWindowCompositionAttribute(this.Handle, ref data);
-        //    Marshal.FreeHGlobal(accentPtr);
-        //}
-
         public static class Acrylic
         {
+
+            public const int WS_SYSMENU = 0x80000;
+            public const int WS_EX_LAYERED = 0x00080000;
+
             [DllImport("user32.dll")]
             internal static extern IntPtr SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
 
@@ -186,28 +216,22 @@ namespace SnapAssistForCentering
                 int accentStructSize = Marshal.SizeOf(accent);
                 IntPtr accentPtr = Marshal.AllocHGlobal(accentStructSize);
                 Marshal.StructureToPtr(accent, accentPtr, false);
-
                 WindowCompositionAttributeData data = new WindowCompositionAttributeData();
                 data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
                 data.SizeOfData = accentStructSize;
                 data.Data = accentPtr;
-
                 SetWindowCompositionAttribute(HWnd, ref data);
-
                 Marshal.FreeHGlobal(accentPtr);
             }
         }
-
-        public const int WS_SYSMENU = 0x80000;
-        public const int WS_EX_LAYERED = 0x00080000;
 
         protected override CreateParams CreateParams
         {
             get
             {
                 CreateParams cParms = base.CreateParams;
-                cParms.Style |= WS_SYSMENU;
-                cParms.ExStyle |= WS_EX_LAYERED;
+                cParms.Style |= Acrylic.WS_SYSMENU;
+                cParms.ExStyle |= Acrylic.WS_EX_LAYERED;
                 return cParms;
             }
         }
